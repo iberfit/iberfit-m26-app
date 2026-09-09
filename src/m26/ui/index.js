f74693aa20ced3097a80c6e0b7f5c629933454d0
iberfit <iberfit.cl@gmail.com>
2026-09-09 10:13:01 -0300

    feat(i18n): export extended surface catalog

--- src/m26/ui/index.js
+++ src/m26/ui/index.js
@@ -8,3 +8,4 @@ export * from '../design/role-surfaces.js';
 export * from './castellano.js';
 export * from './i18n-shell.js';
 export * from './i18n-surface.js';
+export * from './i18n-surface-extra.js';
