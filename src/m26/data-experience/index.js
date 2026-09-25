b300d6e8e9ccbdb719ee129b06c3f2ac332ca690
iberfit <iberfit.cl@gmail.com>
2026-09-25 00:54:48 -0300

    feat(adherence): route longitudinal UI through trajectory insight

--- src/m26/data-experience/index.js
+++ src/m26/data-experience/index.js
@@ -1,3 +1,5 @@
 export * from './echarts-element.js';
 export * from './data-trust.js';
-export * from './longitudinal-ui.js';
+export {__longitudinalUiInternals} from './longitudinal-ui.js';
+export * from './adherence-trajectory.js';
+export {renderLongitudinalDataExperience,__longitudinalAdherenceUiInternals} from './longitudinal-adherence-ui.js';
